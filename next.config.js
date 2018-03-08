7e380d18e828cfcbc9a64215ce3af171e51e6536
lethanhsonthd <lethanhsonthd@aol.com>
2018-03-08 12:10:26 +0700

    fixx

--- next.config.js
+++ next.config.js
@@ -4,5 +4,5 @@ module.exports = {
       "/": { page: "/" }
     };
   },
-  assetPrefix: process.env.NODE_ENV === "production" ? "/khanhngan" : ""
+  assetPrefix: process.env.NODE_ENV === "production" ? "/lovelovelove" : ""
 };
